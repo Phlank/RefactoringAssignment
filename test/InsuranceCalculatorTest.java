71221ee6ea3731868cfe8807da8e7f9b3a423038
Colin <ledbetter.colin.m@gmail.com>
2020-02-13 11:40:51 -0500

    Create tests

--- test/InsuranceCalculatorTest.java
+++ test/InsuranceCalculatorTest.java
@@ -4,4 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InsuranceCalculatorTest {
 
+    InsuranceCalculator calculator = new InsuranceCalculator();
+
+    @Test
+    void low() {
+        assertEquals(1825, calculator.calculateInsurance(5000));
+    }
+
+    @Test
+    void medium() {
+        assertEquals(36600, calculator.calculateInsurance(15000));
+    }
+
+    @Test
+    void high() {
+        assertEquals(78500, calculator.calculateInsurance(50000));
+    }
+
+    @Test
+    void veryHigh() {
+        assertEquals(106000, calculator.calculateInsurance(80000));
+    }
+
 }
